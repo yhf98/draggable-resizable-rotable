1fbfd4fc2f17f4de4b84bbaaccd4a39e270cc8cf
姚恒锋 <1921934563@qq.com>
2023-08-22 18:38:40 +0800

    feat: 仓库初始化

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;">
+      <vue-draggable-resizable :w="400" :h="400" :parent="true" :min-width="10" :min-height="20">
+        <p>vue-draggable-resizable</p>
+      </vue-draggable-resizable>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueDraggableResizable from './components/vue-draggable-resizable'
+import './components/vue-draggable-resizable.css'
+
+export default {
+  name: 'app',
+  components: {
+    VueDraggableResizable
+  }
+}
+</script>
+
+<style>
+  .vdr {
+    border: 1px dashed black;
+  }
+</style>
